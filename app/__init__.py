814e02eb66c6b453ec2157c5937e07cf91cdd475
Juan Carlos Ramos Peroni <juancramosp@gmail.com>
2022-03-23 23:25:14 -0300

    Added Flask-JWT-Extended

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from flask_marshmallow import Marshmallow
 from flask.cli import AppGroup
+from http import HTTPStatus
 
 api = Api()
+jwt = JWTManager()
 ma = Marshmallow()
 
 
 def create_app(config_name=None):
     from config import config
-    from app import db, auth, ticket
+    from app import db, auth, login, ticket
 
     if config_name is None:
         config_name = 'default'
@@ -19,8 +22,10 @@ def create_app(config_name=None):
 
     db.init_app(app)
     api.init_app(app)
+    jwt.init_app(app)
     ma.init_app(app)
     auth.init_app(api)
+    login.init_app(api)
     ticket.init_app(api)
 
     @app.route('/')
@@ -28,6 +33,14 @@ def create_app(config_name=None):
         return {
             'status': 'OK'
         }
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(e):
+        return jsonify({'message': e}), HTTPStatus.UNAUTHORIZED
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({'message': 'Expired Access Token'}), HTTPStatus.UNAUTHORIZED
 
     db_cli = AppGroup('db')
 
