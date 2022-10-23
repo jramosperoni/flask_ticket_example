6a78f9a7fdba5a6b084841fa80d9e6533d537fdd
Juan Carlos Ramos Peroni <juan.c.ramos.p@gmail.com>
2022-10-23 19:08:25 -0300

    Added RoleSchema

--- app/auth/serializers.py
+++ app/auth/serializers.py
@@ -1,13 +1,20 @@
-from marshmallow import validate
-from .models import User
+from marshmallow import fields, validate
+from .models import Role, User
 from app import ma
+
+
+class RoleSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Role
+        fields = ('id', 'name')
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'deleted', 'password', 'role', 'username')
+        fields = ('id', 'first_name', 'last_name', 'deleted', 'password', 'roles', 'username')
         load_instance = True
 
     password = ma.String(load_only=True, required=True)
+    roles = fields.Nested(RoleSchema(many=True))
     username = ma.auto_field(validate=validate.Length(min=3))
